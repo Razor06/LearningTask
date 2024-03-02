4b8a913519cf647565a3e1c3baad981fce6f11da
Razor06 <rashiddoskiev5@mail.ru>
2024-03-02 20:52:34 +0300

    task 1 has been completed

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,6 +2,46 @@
 {
     public static void Main()
     {
-        Console.WriteLine("task1");
+        int age = 25;
+        string name = "Rashid";
+        double temperature = 98.6;
+        bool isAdult = false;
+
+        int birthYear = DateTime.Now.Year - age;
+        string message = $"Привет, меня зовут {name}, мне {age} лет" +
+                         $" и моя температура тела составляет {temperature}" +
+                         $" градусов по Фаренгейту.";
+        Console.WriteLine(message);
+        IsAnAdult(age, isAdult);
+        IsHeat(temperature);
+
+        age += 5;
+        temperature += 2.5;
+    }
+
+    private static void IsHeat(double temperature)
+    {
+        if (temperature > 98.6)
+        {
+            Console.WriteLine("У вас повышенная температура");
+        }
+        else
+        {
+            Console.WriteLine("Ваша температура в норме");
+        }
+    }
+
+    private static void IsAnAdult(int age, bool isAdult)
+    {
+        if (age >= 18)
+        {
+            Console.WriteLine("Вы совершеннолетний");
+            isAdult = true;
+        }
+        else
+        {
+            Console.WriteLine("Вы не совершеннолетний");
+            isAdult = false;
+        }
     }
 }
